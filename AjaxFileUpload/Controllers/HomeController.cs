8f77541761f442b225d517d105a72f43fe66457d
lenovo <lenovo@DESKTOP-BCJATGL>
2022-07-17 20:49:23 +0430

    Add project files.

---
+++ AjaxFileUpload/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using AjaxFileUpload.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace AjaxFileUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment hostingEnvironment;
+        public HomeController(ILogger<HomeController> logger , IWebHostEnvironment hostingEnvironment)
+        {
+            _logger = logger;
+            this.hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IList<IFormFile> files)
+        {
+            foreach (IFormFile source in files)
+            {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img");
+
+                //create folder if not exist
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+
+                //get file extension
+                FileInfo fileInfo = new FileInfo(source.FileName);
+                string fileName = source.FileName + fileInfo.Extension;
+
+                string fileNameWithPath = Path.Combine(path, fileName);
+
+                using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                {
+                    source.CopyTo(stream);
+                }
+            }
+
+            return this.View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AjaxFileUpload/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AjaxFileUpload.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
